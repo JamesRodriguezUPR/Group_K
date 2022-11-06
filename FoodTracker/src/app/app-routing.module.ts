965a095f2e422e57b3f1da54a1bd9fa531cdcdda
Gabriel L <gabriel.luque@upr.edu>
2022-11-06 15:35:48 -0400

    Add food component basic template.

--- FoodTracker/src/app/app-routing.module.ts
+++ FoodTracker/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddFoodComponent } from './add-food/add-food.component';
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes = [
+  //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+  //{ path: 'home', component: HomeComponent },
+  {path: "add-food", component:AddFoodComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
